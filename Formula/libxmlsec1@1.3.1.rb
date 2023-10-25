204eb295ce4bd9bb9127ad1ebfe502517be6110c
bartland <bartland@users.noreply.github.com>
2023-10-25 16:32:53 +1100

    libxmlsec1@1.3.1.rb

---
+++ Formula/libxmlsec1@1.3.1.rb
@@ -0,0 +1,58 @@
+class Libxmlsec1AT131 < Formula
+  desc "XML security library"
+  homepage "https://www.aleksey.com/xmlsec/"
+  url "https://www.aleksey.com/xmlsec/download/xmlsec1-1.3.1.tar.gz"
+  sha256 "10f48384d4fd1afc05fea545b74fbf7c152582f0a895c189f164d55270400c63"
+  license "MIT"
+  revision 1
+
+  livecheck do
+    url "https://www.aleksey.com/xmlsec/download/"
+    regex(/href=.*?xmlsec1[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on xcode: :build
+  depends_on "gnutls" # Yes, it wants both ssl/tls variations
+  depends_on "libgcrypt"
+  depends_on "libxml2"
+  depends_on "openssl@3"
+  uses_from_macos "libxslt"
+
+  # Add HOMEBREW_PREFIX/lib to dl load path
+  patch :DATA
+
+  def install
+    args = ["--disable-dependency-tracking",
+            "--prefix=#{prefix}",
+            "--disable-crypto-dl",
+            "--disable-apps-crypto-dl",
+            "--with-nss=no",
+            "--with-nspr=no",
+            "--enable-mscrypto=no",
+            "--enable-mscng=no",
+            "--with-openssl=#{Formula["openssl@3"].opt_prefix}"]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/xmlsec1", "--version"
+    system "#{bin}/xmlsec1-config", "--version"
+  end
+end
+
+__END__
+diff --git a/src/dl.c b/src/dl.c
+index 6e8a56a..0e7f06b 100644
+--- a/src/dl.c
++++ b/src/dl.c
+@@ -141,6 +141,7 @@ xmlSecCryptoDLLibraryCreate(const xmlChar* name) {
+     }
+
+ #ifdef XMLSEC_DL_LIBLTDL
++    lt_dlsetsearchpath("HOMEBREW_PREFIX/lib");
+     lib->handle = lt_dlopenext((char*)lib->filename);
+     if(lib->handle == NULL) {
+         xmlSecError(XMLSEC_ERRORS_HERE,
